9f9c32cf430f8494d7254cb09a91d5c57b026bc4
Sharan Yalburgi <sharanyalburgi@gmail.com>
2018-01-30 19:09:31 +0530

    Setup Import-Export

--- ARC/main/admin.py
+++ ARC/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+
+from main.models import Student, Course, CourseSlot
+
+@admin.register(Student)
+class StudentAdmin(ImportExportModelAdmin):
+    search_fields = ['bitsId']
+
+
+# admin.site.register(Student, StudentAdmin)
--- ARC/main/models.py
+++ ARC/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    bits_id = models.CharField(max_length=13, primary_key=True)
+
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=10, primary_key=True)
+    # course_slot =
+
+
+class CourseSlot(models.Model):
+    DAYS = {
+        ('M', 'Monday'),
+        ('Tu', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('Th', 'Thursday'),
+        ('F', 'Friday'),
+        ('S', 'Saturday')
+    }
+    days = models.CharField(max_length=2, choices=DAYS)
+    # time_slot = models.CahrField()
