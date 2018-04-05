2faa85574a10b1ba081ec6169105e72443aef66e
rishabkhincha <khincharishab@gmail.com>
2018-04-06 00:17:28 +0530

    Added form for year and sem

--- ARC/main/forms.py
+++ ARC/main/forms.py
@@ -8,3 +8,18 @@ class MapsForm(forms.Form):
         required=True,
         help_text='Which pre-defined map to apply?',
     )
+
+class SingleCDCForm(forms.Form):
+	# YEAR_CHOICES=(('1','1'),('2','2'),('3','3'),('4','4'),('5','5'))
+	# SEM_CHOICES=(('1','1'),('2','2'))
+	Year = forms.IntegerField(
+        # choices=SEM_CHOICES,
+        required=True,
+        help_text='Year',
+    )
+
+	Sem = forms.IntegerField(  
+    	# choices=SEM_CHOICES,
+        required=True,
+        help_text='Sem',
+    )
